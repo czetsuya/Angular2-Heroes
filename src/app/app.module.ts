879b8d43b90633c7e7656c542df0c4695b9f908c
Edward Legaspi <czetsuya@gmail.com>
2017-05-22 23:07:10 +0800

    Completed version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BusyModule } from 'angular2-busy';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+//Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroSearchService } from './hero-search.service'; 
+
+@NgModule( {
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent,
+        HeroSearchComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        BusyModule,
+        InMemoryWebApiModule.forRoot( InMemoryDataService )
+    ],
+    providers: [HeroService, HeroSearchService],
+    bootstrap: [AppComponent]
+} )
 export class AppModule { }
